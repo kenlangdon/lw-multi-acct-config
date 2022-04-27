7d2892efd28f79ed9845fffbf0a967425f0ada88
Ken <ken@lacework.net>
2022-04-27 18:27:19 -0500

    updated README, created aws folder
    Signed-off-by: Ken <ken@lacework.net>

---
+++ terraform/aws/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  alias   = "account-1"
+  profile = "account-1"
+  region  = "us-east-2"
+}
+
+module "account-1" {
+  source  = "lacework/config/aws"
+  version = "0.5"
+
+  providers = {
+    aws = aws.account-1
+  }
+}
+
+provider "aws" {
+  alias   = "account-2"
+  profile = "account-2"
+  region  = "us-east-2"
+}
+
+module "account-2" {
+  source  = "lacework/config/aws"
+  version = "0.5"
+
+  providers = {
+    aws = aws.account-2
+  }
+}
+
+provider "aws" {
+  alias   = "account-3"
+  profile = "account-3"
+  region  = "us-east-2"
+}
+
+module "account-3" {
+  source  = "lacework/config/aws"
+  version = "0.5"
+
+  providers = {
+    aws = aws.account-3
+  }
+}
