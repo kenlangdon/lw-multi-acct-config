ad31bed26006c6c6daef9030b002f495c06bee88
Ken <ken@lacework.net>
2022-04-29 16:10:40 -0500

    added LW Integration Name variable
    Signed-off-by: Ken <ken@lacework.net>

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -7,6 +7,7 @@ provider "aws" {
 module "account-1" {
   source  = "lacework/config/aws"
   version = "0.5"
+  lacework_integration_name = "account-1"
 
   providers = {
     aws = aws.account-1
@@ -22,6 +23,7 @@ provider "aws" {
 module "account-2" {
   source  = "lacework/config/aws"
   version = "0.5"
+  lacework_integration_name = "account-2"
 
   providers = {
     aws = aws.account-2
@@ -37,6 +39,7 @@ provider "aws" {
 module "account-3" {
   source  = "lacework/config/aws"
   version = "0.5"
+  lacework_integration_name = "account-3"
 
   providers = {
     aws = aws.account-3
